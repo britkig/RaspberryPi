b6869f0cbee6b2f0980d7e11be5a9445db7bd76b
BritKig <97478354+britkig@users.noreply.github.com>
2025-11-12 10:41:32 +0000

    Update pin_reset.h

--- pin_reset.h
+++ pin_reset.h
@@ -3,10 +3,11 @@
 struct PinReset : PinInput{
 	PinReset(uint8_t p):PinInput(p,false){
 		WaitForState(false);
-		gpio_set_irq_enabled_with_callback(_p, GPIO_IRQ_EDGE_FALL, true, &_trigger);
+		gpio_set_irq_enabled_with_callback(_p, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, true, &_trigger);
 	}
 private:
 	static void _trigger(uint gpio, uint32_t events){
+		if(!Read()) return;
 		watchdog_enable(0,1);
 		while(true);
 	}
