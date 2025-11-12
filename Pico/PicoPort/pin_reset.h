fdaf7879e154e2ac03a54500e31d98897059a9f8
BritKig <97478354+britkig@users.noreply.github.com>
2025-11-12 16:38:00 +0000

    Update pin_reset.h

--- Pico/PicoPort/pin_reset.h
+++ Pico/PicoPort/pin_reset.h
@@ -1,7 +1,7 @@
 #pragma once	// FOR PI PICO ONLY
 #include "pin_input.h"
-struct PinReset : PinInput{
-	PinReset(uint8_t p):PinInput(p,false){
+struct PinReset : Pin{
+	PinReset(uint8_t p):Pin(p,false){
 		WaitForState(false);
 		gpio_set_irq_enabled_with_callback(_p, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, true, &_trigger);
 	}
