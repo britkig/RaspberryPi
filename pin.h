d346655b836969aa7ed2190f6022cd38a80b8aaa
BritKig <97478354+britkig@users.noreply.github.com>
2025-11-12 10:55:04 +0000

    Update pin.h

--- pin.h
+++ pin.h
@@ -7,3 +7,15 @@ protected:
 		gpio_init(_p=p);
 	}
 };
+#define PICOPORT_A {0,1,2,3}
+#define PICOPORT_AB {0,1,2,3,4,5,6,7}
+#define PICOPORT_B {4,5,6,7}
+#define PICOPORT_BC {4,5,6,7,8,9,10,11}
+#define PICOPORT_C {8,9,10,11}
+#define PICOPORT_CD {8,9,10,11,12,13,14,15}
+#define PICOPORT_D {12,13,14,15}
+#define PICOPORT_DE {12,13,14,15,16,17,18,19}
+//	WARNING: Ports D and E do not share same side of Pico module.
+#define PICOPORT_E {16,17,18,19}
+#define PICOPORT_EF {16,17,18,19,20,21,22,26}
+#define PICOPORT_F {20,21,22,26}
